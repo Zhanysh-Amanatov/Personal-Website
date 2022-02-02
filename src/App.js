3b2cc2215fe95362a764acf310371ec48e043f10
Zhanysh-Amanatov <amanatov.zhanysh22@gmail.com>
2022-02-02 12:44:58 +0600

    removed comments

--- src/App.js
+++ src/App.js
@@ -3,23 +3,19 @@ import { CssBaseline, makeStyles } from "@material-ui/core";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Portfolio from "./components/Portfolio";
-import  {Background}  from "./components/Background";
-import Footer from "./components/Footer"; 
-import Home from './components/Home';
-;
-
-
-
+import { Background } from "./components/Background";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 const useStyles = makeStyles((theme) => ({
   root: {
-    fontFamily: 'Inconsolata',
+    fontFamily: "Inconsolata",
     // display:"flex",
     height: "100%",
     width: "auto",
     //backgroundColor: "#2a2728",
     // backgroundColor: "#30336b",
     backgroundColor: "#0C1E30",
-  },//#0C1E30
+  }, //#0C1E30
   //#172E43
 }));
 export default function App() {
@@ -27,41 +23,14 @@ export default function App() {
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
       {/* <Background /> */}
+      <CssBaseline />
       <Header />
-      <Home/>
- 
+      <Home />
       <AboutMe />
       <Portfolio />
-      <Footer/>
-
+      <Footer />
     </div>
   );
 }
 
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
