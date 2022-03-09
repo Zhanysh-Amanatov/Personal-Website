b34b9c3f9b1870f2e66b94785b8977abea3b1b14
Zhanysh-Amanatov <amanatov.zhanysh22@gmail.com>
2022-03-09 19:24:11 +0600

    added projects to project section

--- src/static/projects.js
+++ src/static/projects.js
@@ -1,22 +1,27 @@
 const projects = [
     {
         title: 'Chess Clocks',
-        description:'Animated login form with validation and interactivity.',
+        description:'Animated chess clocks with sound effect',
         imageUrl:process.env.PUBLIC_URL + '/assets/chess_clocks.png',
         live:process.env.PUBLIC_URL + 'https://chess-clocks.netlify.app/',
         code:process.env.PUBLIC_URL + 'https://github.com/Zhanysh-Amanatov/chess_clocks',
     },
     {
-        title: 'Recipes App',
-        description:'Service for searching different recipes.Data is fetched via API calls.',
-        imageUrl:process.env.PUBLIC_URL + '/assets/project2.jpg',
-        live:process.env.PUBLIC_URL + 'https://www.freecodecamp.org/learn',
+        title: 'Login Form',
+        description:'Animated login form with validation and interactivity.',
+        imageUrl:process.env.PUBLIC_URL + '/assets/login-form.png',
+        live:process.env.PUBLIC_URL + 'https://loginformjs.netlify.app/',
+        code:process.env.PUBLIC_URL + 'https://github.com/Zhanysh-Amanatov/Form-animation',
     },
     {
-        title: 'Find a Chess Coach',
-        description:'Service for searching chess coaches online around the world.Free trial lesson and student community for group learning. ',
-        imageUrl:process.env.PUBLIC_URL + '/assets/project3.jpg',
+        title: 'To Do App',
+        description:'To do app with delelte and mark options',
+        imageUrl:process.env.PUBLIC_URL + '/assets/react-to-do.png',
+        live:process.env.PUBLIC_URL + 'https://todlist12.netlify.app/',
+        code:process.env.PUBLIC_URL + 'https://github.com/Zhanysh-Amanatov/To-Do-List',
     },
 ];
 
 export default projects;
+
+//Service for searching chess coaches online around the world.Free trial lesson and student community for group learning.
